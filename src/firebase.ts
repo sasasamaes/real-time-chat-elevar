5fb57ad98e32b62196c7ea9ebe8df68d157aad52
Francisco Campos <sasasasamaes13@gmail.com>
2023-08-09 17:27:12 -0600

    pryect

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,11 +5,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey:  "AIzaSyAMt1N5H9BoXwoe1V7j3V-Fr96w78qKxVs",
-  authDomain: "superchat-1d920",
-  storageBucket:  "superchat-1d920.appspot.com",
+  apiKey: "AIzaSyAMt1N5H9BoXwoe1V7j3V-Fr96w78qKxVs",
+  authDomain: "superchat-1d920.firebaseapp.com",
+  projectId: "superchat-1d920",
+  storageBucket: "superchat-1d920.appspot.com",
   messagingSenderId: "124404443106",
-  appId: "1:124404443106:web:6e8e179143a11c4a1afbbb",
+  appId: "1:124404443106:web:6e8e179143a11c4a1afbbb"
 };
 
 const app = initializeApp(firebaseConfig);
