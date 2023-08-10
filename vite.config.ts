61b9d52720a3a8c31de5ab4827796104435bd057
Francisco Campos <sasasasamaes13@gmail.com>
2023-08-10 11:29:30 -0600

    /

--- vite.config.ts
+++ vite.config.ts
@@ -27,23 +27,23 @@ export default defineConfig({
         theme_color: '#583D8A',
         icons: [
           {
-            src: 'android-chrome-512x512.png',
+            src: '/android-chrome-512x512.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'android-chrome-512x512.png',
+            src: '/android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           },
           {
-            src: 'android-chrome-512x512.png',
+            src: '/android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any'
           },
           {
-            src: 'android-chrome-512x512.png',
+            src: '/android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'maskable'
